2a0857ce3af1ea0746aadb5028496402d3fbf95e
havario <157877551+havario@users.noreply.github.com>
2026-05-05 14:07:45 +0800

    -
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- .github/bump-go.sh
+++ .github/bump-go.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2026 honeok <i@honeok.com>
 
-set -eExo pipefail
+set -eEo pipefail
 
 die() {
     printf >&2 "Error: %s\n" "$*"
@@ -10,51 +10,61 @@ die() {
 }
 
 curl() {
-    local RET
+    local rc
+
     # 添加 --fail 不然404退出码也为0
     # 32位cygwin已停止更新, 证书可能有问题, 添加 --insecure
     # centos7 curl 不支持 --retry-connrefused --retry-all-errors 因此手动 retry
     for ((i = 1; i <= 5; i++)); do
         command curl --connect-timeout 10 --fail --insecure "$@"
-        RET="$?"
-        if [ "$RET" -eq 0 ]; then
+        rc="$?"
+        if [ "$rc" -eq 0 ]; then
             return
         else
             # 403 404 错误或达到重试次数
-            if [ "$RET" -eq 22 ] || [ "$i" -eq 5 ]; then
-                return "$RET"
+            if [ "$rc" -eq 22 ] || [ "$i" -eq 5 ]; then
+                return "$rc"
             fi
-            sleep 1
+            sleep 0.5
         fi
     done
 }
 
-is_china() {
+is_in_china() {
     if [ -z "$COUNTRY" ]; then
-        if ! COUNTRY="$(curl -Ls -4 http://www.qualcomm.cn/cdn-cgi/trace | grep '^loc=' | cut -d= -f2 | grep .)"; then
+        # www.prologis.cn
+        # www.autodesk.com.cn
+        # www.keysight.com.cn
+        if ! COUNTRY="$(curl -L -4 http://www.qualcomm.cn/cdn-cgi/trace | grep '^loc=' | cut -d= -f2 | grep .)"; then
             die "Can not get location."
         fi
-        echo >&1 "Location: $COUNTRY"
+        echo >&2 "Location: $COUNTRY"
     fi
     [ "$COUNTRY" = CN ]
 }
 
-if is_china; then
-    GO_MIRROR="golang.google.cn"
-else
-    GO_MIRROR="go.dev"
-fi
+main() {
+    local go_mirror official_var work_dir local_ver
 
-# 官方版本
-OFFICIAL_VER="$(curl -Ls "https://$GO_MIRROR/dl/?mode=json" | awk '/"version"/ && !p { sub(/.*"go/, ""); sub(/".*/, ""); print; p=1 }')"
-
-find "$PWD" -type f -name "go.mod" -not -path '*/.*' | while read -r f; do
-    WORK_DIR="$(dirname "$f")"
-
-    cd "$WORK_DIR" || exit 1
-    LOCAL_VER="$(awk '/^[[:space:]]*go[[:space:]]+[0-9]+\.[0-9]+(\.[0-9]+)?$/ {print $2; exit}' go.mod || true)" # 本地版本
-    if [ "$LOCAL_VER" != "$OFFICIAL_VER" ]; then
-        sed -i -E "s#^[[:space:]]*go[[:space:]]+[0-9]+\.[0-9]+(\.[0-9]+)?#go $OFFICIAL_VER#" go.mod
-        go mod tidy
+    if is_in_china; then
+        go_mirror="golang.google.cn"
+    else
+        go_mirror="go.dev"
     fi
-done
+
+    # 官方版本
+    official_var="$(curl -L "https://$go_mirror/dl/?mode=json" | awk '/"version"/ && !p { sub(/.*"go/, ""); sub(/".*/, ""); print; p=1 }')"
+
+    find "$PWD" -type f -name "go.mod" -not -path '*/.*' | while read -r f; do
+        work_dir="$(dirname "$f")"
+
+        cd "$work_dir" || exit 1
+        local_ver="$(awk '/^[[:space:]]*go[[:space:]]+[0-9]+\.[0-9]+(\.[0-9]+)?$/ {print $2; exit}' go.mod || true)" # 本地版本
+        if [ "$local_ver" != "$official_var" ]; then
+            sed -E -i "s#^[[:space:]]*go[[:space:]]+[0-9]+\.[0-9]+(\.[0-9]+)?#go $official_var#" go.mod
+            go mod tidy
+        fi
+    done
+}
+
+main
